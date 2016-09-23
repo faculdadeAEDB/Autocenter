4dae3dd6a198d8b16cf3ebd51927243261f897fb
Roberto <robertooliveiraestudo@gmail.com>
2016-09-22 22:36:44 -0300

    Funcionatio repository and controller modafóquis!

---
+++ Controllers/FuncionarioController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Repositories;
+using Domain.Entities;
+
+namespace Controllers
+{
+    public class FuncionarioController
+    {
+        FuncionarioRepository funcionarioRepository;
+
+        public FuncionarioController()
+        {
+            funcionarioRepository = new FuncionarioRepository();
+        }
+
+        public Funcionario Editar(Funcionario obj)
+        {
+            return funcionarioRepository.Editar(obj);
+        }
+
+        public Funcionario Excluir(Funcionario obj)
+        {
+            return funcionarioRepository.Excluir(obj);
+        }
+
+        public List<Funcionario> Obter()
+        {
+            return funcionarioRepository.Obter();
+        }
+
+        public Funcionario Obter(int id)
+        {
+            return funcionarioRepository.Obter(id);
+        }
+
+        public Funcionario Salvar(Funcionario obj)
+        {
+            return funcionarioRepository.Salvar(obj);
+        }
+    }
+}
