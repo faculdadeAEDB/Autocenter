f843492e4429492044fe265767f220c4fadb06af
Roberto <robertooliveiraestudo@gmail.com>
2016-09-23 20:29:57 -0300

    Criando ordemServicoController e ordemServicoRepository

--- Controllers/OrdemServicoController.cs
+++ Controllers/OrdemServicoController.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataAccess.Repositories;
+using Domain.Entities;
 
 namespace Controllers
 {
-    class OrdemServicoController
+    public class OrdemServicoController
     {
+        OrdemServicoRepository ordemServicoRepository;
+
+        public OrdemServicoController()
+        {
+            ordemServicoRepository = new OrdemServicoRepository();
+        }
+
+        public OrdemServico Editar(OrdemServico obj)
+        {
+            return ordemServicoRepository.Editar(obj);
+        }
+
+        public OrdemServico Excluir(OrdemServico obj)
+        {
+            return ordemServicoRepository.Excluir(obj);
+        }
+        public List<OrdemServico> Obter()
+        {
+            return ordemServicoRepository.Obter();
+        }
+
+        public OrdemServico Obter(int id)
+        {
+            return ordemServicoRepository.Obter(id);
+        }
+
+        public OrdemServico Salvar(OrdemServico obj)
+        {
+            return ordemServicoRepository.Salvar(obj);
+        }
     }
 }
