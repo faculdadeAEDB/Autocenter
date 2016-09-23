fd590ee7db9421a30bffaf25109c98b40a057491
Roberto <robertooliveiraestudo@gmail.com>
2016-09-22 23:27:06 -0300

    Venda controllers and repository

--- Controllers/VendaController.cs
+++ Controllers/VendaController.cs
@@ -3,10 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataAccess.Repositories;
+using Domain.Entities;
 
 namespace Controllers
 {
-    class VendaController
+    public class VendaController
     {
+        VendaRepository vendaRepository;
+
+        public VendaController()
+        {
+            vendaRepository = new VendaRepository();
+        }
+
+        public Venda Editar(Venda obj)
+        {
+            return vendaRepository.Editar(obj);
+        }
+
+        public Venda Excluir(Venda obj)
+        {
+            return vendaRepository.Excluir(obj);
+        }
+
+        public List<Venda> Obter()
+        {
+            return vendaRepository.Obter();
+        }
+
+        public Venda Obter(int id)
+        {
+            return vendaRepository.Obter(id);
+        }
+
+        public Venda Salvar(Venda obj)
+        {
+            return vendaRepository.Salvar(obj);
+        }
     }
 }
--- DataAccess/Repositories/VendaRepository.cs
+++ DataAccess/Repositories/VendaRepository.cs
@@ -3,10 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Domain.Entities;
+using System.Data.Entity;
 
 namespace DataAccess.Repositories
 {
-    class VendaRepository
+    public class VendaRepository : IRepository<Venda>
     {
+        public Venda Editar(Venda obj)
+        {
+            using (DataContext c = new DataContext())
+            {
+                c.Vendas.Attach(obj);
+                c.Entry(obj).State = EntityState.Modified;
+                c.SaveChanges();
+
+                return obj;
+            }
+        }
+
+        public Venda Excluir(Venda obj)
+        {
+            using (DataContext c = new DataContext())
+            {
+                Venda vendaExcluido = c.Vendas.Remove(obj);
+                c.SaveChanges();
+                return vendaExcluido;
+            }
+        }
+
+        public List<Venda> Obter()
+        {
+            using (DataContext c = new DataContext())
+            {
+                return c.Vendas
+                    .Include(Venda => Venda.Receitas)
+                    .Include(Venda => Venda.OrdemServico)
+                    .ToList();
+            }
+        }
+
+        public Venda Obter(int id)
+        {
+            using (DataContext c = new DataContext())
+            {
+                return c.Vendas.Where(item => item.VendaId == id).First();
+            }
+        }
+
+        public Venda Salvar(Venda obj)
+        {
+            using (DataContext c = new DataContext())
+            {
+                Venda novaVenda = c.Vendas.Add(obj);
+                c.SaveChanges();
+                return novaVenda;
+            }
+        }
     }
 }
