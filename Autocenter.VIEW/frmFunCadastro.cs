3aab0c273971041041048c2b075a88ca5a980710
Roberto <robertooliveiraestudo@gmail.com>
2016-09-23 21:24:42 -0300

    Add funcionario

--- Autocenter.VIEW/frmFunCadastro.cs
+++ Autocenter.VIEW/frmFunCadastro.cs
@@ -7,19 +7,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Domain.Entities;
+using Controllers;
+
 
 namespace Autocenter.VIEW
 {
     public partial class frmFunCadastro : Form
     {
+        FuncionarioController controller;
+
         public frmFunCadastro()
         {
             InitializeComponent();
+            controller = new FuncionarioController();
         }
 
         private void frmFunCadastro_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnFuncSalvar_Click(object sender, EventArgs e)
+        {
+            Funcionario funcionario = new Funcionario();
+            funcionario.Nome = txtFunNome.Text;
+            funcionario.CPF = txtFuncCPF.Text;
+            funcionario.Funcao = txtFuncFuncao.Text;
+
+            controller.Salvar(funcionario);
         }
     }
 }
